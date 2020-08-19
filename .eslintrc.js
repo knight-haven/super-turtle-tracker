e94249f4aed1bbfea101fc603c5a4a33a2ad15d6
Charles Kornoelje <charleskornoelje@gmail.com>
2020-08-19 12:05:09 -0400

    configure prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
