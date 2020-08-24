f29f5573f435532de51e43d0aa4b417db257adce
Jonathan Ellis <jde27@students.calvin.edu>
2020-08-24 13:38:12 -0400

    Fix undefined when loading

--- client-super-turtle-tracker/src/components/reusables/TurtleList.tsx
+++ client-super-turtle-tracker/src/components/reusables/TurtleList.tsx
@@ -1,36 +1,27 @@
 import useAxios from "axios-hooks";
 import * as React from "react";
 import { ScrollView } from "react-native";
+import { View } from ".";
 import { BACKEND_SECRET, BASE_URL } from "../../../env";
 import { Turtle } from "../../utils/interfaces/Turtle";
 import { TurtleListItem } from "./TurtleListItem";
 
 export const TurtleList = (): JSX.Element => {
   // const [turtleList, setTurtleList] = useState<Turtle[]>([]);
-  const [{ data, loading, error }, refetch] = useAxios({
+  const [{ data, loading, error, response }, refetch] = useAxios({
     headers: { Authorization: `Bearer ${BACKEND_SECRET}` },
     url: `${BASE_URL}/turtle`,
   });
 
-  // const getTurtles = () => {
-  //   axios
-  //     .get<Turtle[]>(`${BASE_URL}/turtle`, {
-  //       headers: { Authorization: `Bearer ${BACKEND_SECRET}` },
-  //     })
-  //     .then((response) => {
-  //       setTurtleList(response.data);
-  //     });
-  // };
-
-  // useEffect(() => {
-  //   getTurtles();
-  // }, []);
-
   return (
-    <ScrollView>
-      {data.map((item: Turtle) => {
-        return <TurtleListItem key={item.id} turtle={item} />;
-      })}
-    </ScrollView>
+    <View>
+      {loading || error ? undefined : (
+        <ScrollView>
+          {data.map((item: Turtle) => {
+            return <TurtleListItem key={item.id} turtle={item} />;
+          })}
+        </ScrollView>
+      )}
+    </View>
   );
 };
