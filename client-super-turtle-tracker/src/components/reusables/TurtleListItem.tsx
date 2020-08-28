f8c921ebe7c98a7137eb1447f3647d2b24e571f7
Jonathan Ellis <jde27@students.calvin.edu>
2020-08-28 13:49:57 -0400

    Resolve some comments from review

--- client-super-turtle-tracker/src/components/reusables/TurtleListItem.tsx
+++ client-super-turtle-tracker/src/components/reusables/TurtleListItem.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Avatar, List } from "react-native-paper";
-import { View } from ".";
 import { firebase } from "../../../env";
 import { capitalizeFirstLetter } from "../../utils/helpers/functions";
 import { Turtle } from "../../utils/interfaces/Turtle";
@@ -14,14 +13,14 @@ const styles = StyleSheet.create({
   },
 });
 
-export const TurtleListItem = ({ turtle }: { turtle: Turtle }): JSX.Element => {
-  // TODO: Store url in database if possible
-  const getPhoto = async (photoName: string): Promise<string> => {
-    const ref = firebase.storage().ref().child(`images/${photoName}`);
-    const url = await ref.getDownloadURL();
-    return url;
-  };
+// TODO: Store url in database if possible
+const getPhoto = async (photoName: string): Promise<string> => {
+  const ref = firebase.storage().ref().child(`images/${photoName}`);
+  const url = await ref.getDownloadURL();
+  return url;
+};
 
+export const TurtleListItem = ({ turtle }: { turtle: Turtle }): JSX.Element => {
   const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
@@ -31,20 +30,20 @@ export const TurtleListItem = ({ turtle }: { turtle: Turtle }): JSX.Element => {
   }, [turtle.avatar]);
 
   return (
-    <View>
-      {imageUrl === "" ? undefined : (
-        <List.Item
-          description={capitalizeFirstLetter(turtle.sex)}
-          left={(props) => {
-            return <Avatar.Image {...props} source={{ uri: imageUrl }} />;
-          }}
-          right={(props) => {
-            return <List.Icon {...props} icon="chevron-right" />;
-          }}
-          style={styles.item}
-          title={turtle.mark}
-        />
-      )}
-    </View>
+    <List.Item
+      description={capitalizeFirstLetter(turtle.sex)}
+      left={
+        imageUrl === ""
+          ? undefined
+          : (props) => {
+              return <Avatar.Image {...props} source={{ uri: imageUrl }} />;
+            }
+      }
+      right={(props) => {
+        return <List.Icon {...props} icon="chevron-right" />;
+      }}
+      style={styles.item}
+      title={turtle.mark}
+    />
   );
 };
