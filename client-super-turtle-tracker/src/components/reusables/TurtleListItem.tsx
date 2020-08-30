7fe35269bbb453bf57129c0c4821bbbaf4c4fefd
Jonathan Ellis <jde27@students.calvin.edu>
2020-08-30 15:41:24 -0400

    Get the most recent image as avatar

--- client-super-turtle-tracker/src/components/reusables/TurtleListItem.tsx
+++ client-super-turtle-tracker/src/components/reusables/TurtleListItem.tsx
@@ -16,7 +16,7 @@ export const TurtleListItem = ({ turtle }: { turtle: Turtle }): JSX.Element => {
     <List.Item
       description={capitalizeFirstLetter(turtle.sex)}
       left={(props) => {
-        return <Avatar.Image {...props} source={{ uri: turtle.url }} />;
+        return <Avatar.Image {...props} source={turtle.url === null ? {} : { uri: turtle.url }} />;
       }}
       right={(props) => {
         return <List.Icon {...props} icon="chevron-right" />;
