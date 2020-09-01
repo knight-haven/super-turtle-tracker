663658e06f327d7eef841ff89c39f2577233f694
Jonathan Ellis <jde27@students.calvin.edu>
2020-09-01 09:11:32 -0400

    small changes

--- client-super-turtle-tracker/src/utils/constants/Contexts.ts
+++
@@ -1,9 +0,0 @@
-// import { NavigationProp } from "@react-navigation/native";
-// import { ListParamList } from "utils";
-// import { createContext } from "react";
-
-// export type ListScreenNavigationProp = NavigationProp<ListParamList, "ListScreen">;
-
-// export const ListScreenNavigationContext = createContext<undefined | ListScreenNavigationProp>(
-//   undefined,
-// );
